19dd2659566a41c1fb31582230dd5af22d6682f2
양현미 <53929448+hmyanghm@users.noreply.github.com>
2020-02-04 20:52:48 +0900

    200204 20:52

--- src/main/java/com/moneyhub/web/cus/mappers/AccountMapper.java
+++ src/main/java/com/moneyhub/web/cus/mappers/AccountMapper.java
@@ -9,5 +9,5 @@ public interface AccountMapper {
 	
 	public void createAcc(Account acc);
 	public int existAcc(String acc);
-	public Account getAcc(Account acc);
+	public Account getAcc(String t);
 }
