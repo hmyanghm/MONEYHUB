d15ea3ec35894dc48b7dab3a8e727c51f29ee8ef
양현미 <53929448+hmyanghm@users.noreply.github.com>
2020-02-11 10:51:24 +0900

    200211 10:51

--- src/main/java/com/moneyhub/web/admin/AdminCtrl.java
+++ src/main/java/com/moneyhub/web/admin/AdminCtrl.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.moneyhub.web.cus.util.CustomerSha256;
 import com.moneyhub.web.pxy.Box;
 import com.moneyhub.web.pxy.Proxy;
 
@@ -46,6 +48,7 @@ public class AdminCtrl extends Proxy{
 	
 	@PostMapping("/memberUpdate")
 	public Map<?, ?> memberListUpdate(@RequestBody Admin param){
+		param.setCpwd(CustomerSha256.encrypt(param.getCpwd()));
 		Consumer<Admin> c = o -> adminMapper.memberUpdate(o);
 		c.accept(param);
 		box.clear();
